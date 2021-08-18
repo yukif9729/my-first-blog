2c0233696008987e850383f5dacec22d25f7327a
Yukinori Fukuda <yukif@po.minc.ne.jp>
2021-08-18 11:29:11 +0900

    Added views to create/edit blog post inside the site.

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,6 +8,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/yukif9729/djangogirls/mysite'
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
