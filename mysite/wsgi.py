6f0d4853c536820830169e38976f9116b4cfeb2b
Yukinori Fukuda <yukif@po.minc.ne.jp>
2021-08-18 15:17:29 +0900

    Added views to create/edit blog post inside the site.

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -14,11 +14,11 @@ path = '/home/yukif9729/yukif9729.pythonanywhere.com'
 if path not in sys.path:
     sys.path.append(path)
 
-"""
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 """
 os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
-
+"""
 
 from django.core.wsgi import get_wsgi_application
 
